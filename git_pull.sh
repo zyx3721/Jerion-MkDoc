716a4cfd868cf43d87a92ae5398518df7b719cb7
zyx3721 <whizhzl@gmail.com>
2024-11-25 18:26:02 +0800

    提交所有更改，包括新增文件

--- git_pull.sh
+++ git_pull.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-# 日志输出
-function echo_log_info() {
-    echo -e "$(date +'%F %T') - [Info] $*\n"
+LOG_FILE="/data/Mkdocs-material/Jerion-Mkdocs/pull_code.log"
+
+echo_log_info() {
+    echo "$(date +'%F %T') - [INFO] $*" | tee -a "$LOG_FILE"
 }
-function echo_log_warn() {
-    echo -e "$(date +'%F %T') - [Warn] $*\n"
-    exit 1
-}
-function echo_log_error() {
-    echo -e "$(date +'%F %T') - [Error] $*\n"
+
+echo_log_error() {
+    echo "$(date +'%F %T') - [ERROR] $*" | tee -a "$LOG_FILE"
     exit 1
 }
 
-function git_pull(){
+git_pull(){
     cd /data/Mkdocs-material/Jerion-Mkdocs
     if ! git pull origin master >/dev/null 2>&1; then
         # 如果拉取失败，发送 webhook 通知
@@ -21,10 +19,35 @@ function git_pull(){
         '{"text":"Git pull failed on /data/Mkdocs-material/Jerion-Mkdocs"}' \
         https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=05154f52-3b88-4a35-a143-ef7d8f7494f1
 
-        echo_log_error "Code pull failed" >> /data/Mkdocs-material/Jerion-Mkdocs/pull_code.log
+        echo_log_error "Code pull failed"
     else
-        echo_log_info "Code pulled successfully" >> /data/Mkdocs-material/Jerion-Mkdocs/pull_code.log
+        echo_log_info "Code pulled successfully"
     fi
+
+    rm -rf /tmp/mkdosc_* && systemctl restart mkdocs
+    [ $? -eq 0 ] && echo_log_info "Mkdocs Service restart successfully!" || echo_log_error "Mkdocs Service restart fail!"
 }
 
-git_pull
+delete_log() {
+    THREE_DAYS_AGO=$(date -d "3 days ago" +%Y-%m-%d)
+    TEMP_FILE=$(mktemp) # 存储保留日志
+
+    awk -v date="$THREE_DAYS_AGO" '{
+        log_date = substr($0, 1, 10); # 提取日志的日期部分 YYYY-MM-DD
+        if (log_date >= date) print $0;
+    }' "$LOG_FILE" > "$TEMP_FILE"
+
+    mv "$TEMP_FILE" "$LOG_FILE"
+    echo_log_info "已清除超过3天的日志。"
+
+    git add . >/dev/null 2>&1
+    git commit -m "提交所有更改，包括新增文件" >/dev/null 2>&1
+    git push origin master >/dev/null 2>&1
+}
+
+main() {
+    git_pull
+    delete_log
+}
+
+main
